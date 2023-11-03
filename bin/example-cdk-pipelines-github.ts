8119c5e90c079b510fe646d2682af2710bfa38e8
Michael Kaiser <awsmikek@amazon.com>
2023-11-03 13:14:42 -0500

    Initial Commit

---
+++ bin/example-cdk-pipelines-github.ts
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { ShellStep } from 'aws-cdk-lib/pipelines';
+import { ExampleCdkPipelinesGithubStack } from '../lib/example-cdk-pipelines-github-stack';
+import { MyGitHubActionRole } from '../lib/my-github-action-role';
+import { AwsCredentials, GitHubWorkflow } from 'cdk-pipelines-github';
+import { MyStage } from '../lib/pipeline';
+
+const app = new cdk.App();
+
+const pipeline = new GitHubWorkflow(app, 'Pipeline', {
+  synth: new ShellStep('Build', {
+    commands: [
+      'yarn install',
+      'yarn build',
+    ],
+
+  }),
+  awsCreds: AwsCredentials.fromOpenIdConnect({
+    gitHubActionRoleArn: 'arn:aws:iam::887227680077:role/GitHubActionRole',
+  }),
+});
+
+pipeline.addStageWithGitHubOptions(new MyStage(app, 'Dev', {
+  env: {
+    account: '887227680077',
+    region: 'us-east-2'
+  }
+
+}))
+
+app.synth()
+
---
+++ lib/my-github-action-role.ts
@@ -0,0 +1,13 @@
+import { App, Stack, StackProps } from 'aws-cdk-lib';
+import { GitHubActionRole } from 'cdk-pipelines-github';
+import { Construct } from 'constructs';
+
+export class MyGitHubActionRole extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const provider = new GitHubActionRole(this, 'github-action-role', {
+    });
+  }
+}
+
